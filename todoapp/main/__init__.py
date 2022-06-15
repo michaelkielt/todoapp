83543fc6c09e7581afb3bf86f478e9154bde4734
40156896 <michaelkielt@gmail.com>
2022-06-15 21:15:03 +0100

    added connection string to init.py

--- todoapp/main/__init__.py
+++ todoapp/main/__init__.py
@@ -5,6 +5,8 @@ from ..extensions import mongo
 def create_app():
     app = Flask(__name__)
 
+    app.config['MONGO_URL'] = 'mongodb+srv://michaelkielt:1234@cluster0.elzid.mongodb.net/todoappretryWrites=true&w=majority'
+
     mongo.init_app(app)
 
     return app
