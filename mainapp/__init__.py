ea35a7187de60bb5d2080e34c2af5fea144e782b
40156896 <michaelkielt@gmail.com>
2022-06-24 18:01:16 +0100

    Fixed mongo URI connection string

--- mainapp/__init__.py
+++ mainapp/__init__.py
@@ -6,7 +6,7 @@ from .extensions import mongo
 def create_app():
     app = Flask(__name__)
 
-    app.config['MONGO_URI'] = 'mongodb+srv://michaelkielt:1234@cluster0.elzid.mongodb.net/todoappretryWrites=true&w=majority'
+    app.config['MONGO_URI'] = 'mongodb+srv://michaelkielt:1234@cluster0.elzid.mongodb.net/todoapp?retryWrites=true&w=majority'
 
     mongo.init_app(app)
 
--- mainapp/main/routes.py
+++ mainapp/main/routes.py
@@ -1,5 +1,3 @@
-from crypt import methods
-from pickletools import read_string1
 from flask import Blueprint, redirect, render_template, url_for, request
 from mainapp.extensions import mongo
 
@@ -13,6 +11,6 @@ def index():
 @main.route('/add_todo', methods=['POST'])
 def add_todo():
     todos_collection = mongo.db.todos
-    todo_item = request.form.get('new_todo')
+    todo_item = request.form.get('new-todo')
     todos_collection.insert_one({'text' : todo_item, 'complete' : False})
     return redirect(url_for('main.index'))
