490dfa6dc9bc6359b056f58230556eef50de2f46
40156896 <michaelkielt@gmail.com>
2022-06-24 18:36:02 +0100

    created function for completing todos

--- mainapp/main/routes.py
+++ mainapp/main/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, redirect, render_template, url_for, request
 from mainapp.extensions import mongo
+from bson.objectid import ObjectId
 
 main = Blueprint('main', __name__)
 
@@ -8,7 +9,7 @@ main = Blueprint('main', __name__)
 def index():
     todos_collection = mongo.db.todos
     todos = todos_collection.find()
-    return render_template ('index.html', todos=todos)
+    return render_template('index.html', todos=todos)
 
 @main.route('/add_todo', methods=['POST'])
 def add_todo():
@@ -16,3 +17,12 @@ def add_todo():
     todo_item = request.form.get('new-todo')
     todos_collection.insert_one({'text' : todo_item, 'complete' : False})
     return redirect(url_for('main.index'))
+
+@main.route('/complete_todo/<oid>')
+def complete_todo(oid):
+    todos_collection = mongo.db.todos
+    todo_item = todos_collection.find_one({'_id' : ObjectId(oid)})
+    todo_item['complete'] = True
+    todos_collection.save(todo_item)
+    return redirect(url_for('main.index'))
+   
