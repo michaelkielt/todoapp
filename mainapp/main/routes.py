3f7218ebc032454e0d6f57e1c61a5eb3b7c8a281
Michael <89637624+michaelkielt@users.noreply.github.com>
2024-01-24 11:15:48 +0000

    Adds docstrings to routes.py

--- mainapp/main/routes.py
+++ mainapp/main/routes.py
@@ -4,34 +4,62 @@ from bson.objectid import ObjectId
 
 main = Blueprint('main', __name__)
 
-
-@main.route('/')
 def index():
+    """
+    Renders the main index page with a list of todos.
+
+    Returns:
+        str: Rendered HTML template.
+    """
     todos_collection = mongo.db.todos
     todos = todos_collection.find()
     return render_template('index.html', todos=todos)
 
-@main.route('/add_todo', methods=['POST'])
 def add_todo():
+    """
+    Adds a new todo item to the database.
+
+    Returns:
+        redirect: Redirects to the main index page.
+    """
     todos_collection = mongo.db.todos
     todo_item = request.form.get('new-todo')
-    todos_collection.insert_one({'text' : todo_item, 'complete' : False})
+    todos_collection.insert_one({'text': todo_item, 'complete': False})
     return redirect(url_for('main.index'))
 
-@main.route('/complete_todo/<oid>')
 def complete_todo(oid):
+    """
+    Marks a specific todo item as complete.
+
+    Args:
+        oid (str): Object ID of the todo item.
+
+    Returns:
+        redirect: Redirects to the main index page.
+    """
     todos_collection = mongo.db.todos
     todos_collection.find_one_and_update({"_id": ObjectId(oid)}, {"$set": {"complete": True}})
     return redirect(url_for('main.index'))
-   
-@main.route('/delete_completed')
+
 def delete_completed():
+    """
+    Deletes all completed todo items.
+
+    Returns:
+        redirect: Redirects to the main index page.
+    """
     todos_collection = mongo.db.todos
-    todos_collection.delete_many({'complete' : True})
+    todos_collection.delete_many({'complete': True})
     return redirect(url_for('main.index'))
 
-@main.route('/delete_all')
 def delete_all():
+    """
+    Deletes all todo items.
+
+    Returns:
+        redirect: Redirects to the main index page.
+    """
     todos_collection = mongo.db.todos
     todos_collection.delete_many({})
     return redirect(url_for('main.index'))
+
